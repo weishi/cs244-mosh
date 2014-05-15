2687e4db1c16bd2f7b23d08688d84f76a58b5b4c
Ubuntu <ubuntu@ip-172-31-5-237.us-west-2.compute.internal>
2014-05-15 23:38:09 +0000

    add package installation in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Checking dependency..."
+apt-get install mosh libio-pty-easy-perl
+
 # Exit on any failure
 set -e
 
@@ -18,29 +21,23 @@ start=`date`
 exptid=`date +%b%d-%H:%M`
 
 rootdir=buffersizing-$exptid
-plotpath=util
-iperf=~/iperf-patched/src/iperf
 
-# TODO: change the interface name for which queue size is adjusted
-# Links are numbered as switchname-eth1,2,etc in the order they are
-# added to the topology.
-iface=s0-eth3
+echo -e "\nStarting mosh experiment..."
+exit
 
 for run in 1; do
-for flows_per_host in 1 2 5 10 50 100 200 300 400; do
-	dir=$rootdir/nf$flows_per_host-r$run
+dir=$rootdir/nf$flows_per_host-r$run
 
 	python buffersizing.py --bw-host 1000 \
-		--bw-net 62.5 \
-		--delay 43.5 \
-		--dir $dir \
-		--nflows $flows_per_host \
-		-n 3 \
-		--iperf $iperf
+	--bw-net 62.5 \
+	--delay 43.5 \
+	--dir $dir \
+	--nflows $flows_per_host \
+	-n 3 \
+	--iperf $iperf
 
 	python $plotpath/plot_queue.py -f $dir/qlen_$iface.txt -o $dir/q.png
 	python $plotpath/plot_tcpprobe.py -f $dir/tcp_probe.txt -o $dir/cwnd.png --histogram
-done
 done
 
 cat $rootdir/*/result.txt | sort -n -k 1
